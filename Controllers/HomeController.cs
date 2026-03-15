812bd29307449e43caaaa722edaf61993d1b2b06
harsh <harsh@HG>
2026-03-14 21:42:32 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IIS_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hello World Get");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok("Hello World Post");
+
+        }
+    }
+}
