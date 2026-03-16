8323c4867cfee33a30394a6b03267420b217a99a
Harshdotnet <harshpatel.3630@gmail.com>
2026-03-16 13:55:38 -0400

    cicd updates

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,12 @@ namespace IIS_Test.Controllers
             return Ok("Hello World Get");
         }
 
+        [HttpGet("name")]
+        public async Task<IActionResult> GetName()
+        {
+            return Ok("My name is Harsh.");
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post()
         {
