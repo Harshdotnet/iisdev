5b12bdab15bc7b53fb087f0b5ccf6ab7ef6619f7
Harshdotnet <harshpatel.3630@gmail.com>
2026-03-16 14:47:18 -0400

    student update

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IIS_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
